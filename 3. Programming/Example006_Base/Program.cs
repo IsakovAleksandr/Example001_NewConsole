9d0adef191831b51fbe1519d764f944a2af7c9ec
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-02 23:57:33 +0300

    add Example006_Base

---
+++ 3. Programming/Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 4;
+int b = 5;
+int c = 8;
+int d = 1;
+int e = 9;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("макимальное значение = ");
+Console.Write(max);
