f8f53df402a4b57017606f44dccde33db79566cb
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-07 01:59:49 +0300

    add Example010_MethodArray

---
+++ 3. Programming/Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 34, 23, 56, 20, 82, 45, 38, 67, 20 };
+int n = array.Length;
+int find = 20;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
